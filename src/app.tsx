236a9c59fdd8cd30f3d67ec2e3c744f61776ae93
wushikang <1853316945@qq.com>
2024-11-15 17:13:21 +0800

    'feat:添加说明'

--- src/app.tsx
+++ src/app.tsx
@@ -26,8 +26,8 @@ export default function App() {
               src={`https://img.shields.io/badge/npm-${version}-brightgreen`}
               alt="NPM version"
             />
-             <a target='_blank' href='https://wskang12138.github.io/wsk-icons/'>GitHub</a>
           </a>
+          <a target='_blank' href='https://wskang12138.github.io/wsk-icons/'>GitHub</a>
           <span>搜索图标可使用<kbd>crtl</kbd>+<kbd>f</kbd>
           </span>
           <span>安装使用: npm install wskicons</span>
